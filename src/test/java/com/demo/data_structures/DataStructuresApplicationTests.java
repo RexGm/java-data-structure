43cb0b43b9fc3899ef69d621cdf3596c851e688f
rexgm <serkaanaltundag@gmail.com>
2025-12-20 19:57:18 +0300

    Initial commit, Readme added

---
+++ src/test/java/com/demo/data_structures/DataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.data_structures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
